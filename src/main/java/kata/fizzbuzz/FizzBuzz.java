9dd22cdd0f0d47079ce122b045c05a542c6617ca
alectunbridge <alec.tunbridge@gmail.com>
2016-07-19 13:01:55 +0100

    Learning to code's first kata.

--- src/main/java/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/kata/fizzbuzz/FizzBuzz.java
@@ -2,8 +2,24 @@ package kata.fizzbuzz;
 
 public class FizzBuzz {
 
-	public String play(int number) {
-		return Integer.toString(number);
-	}
 
+	public String playNumber(int i) {
+
+		if(i%3 == 0 && i%5 == 0){
+			return "fizzbuzz";
+
+		}
+		if(i%3 == 0){
+			return "fizz";
+
+		}
+
+		if(i%5 == 0){
+			return "buzz";
+
+		}
+		return Integer.toString(i);
+
+
+	}
 }
--- src/test/java/kata/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/kata/fizzbuzz/FizzBuzzTest.java
@@ -4,11 +4,49 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
 public class FizzBuzzTest {
     @Test
-    public void play1() throws Exception {
-        assertThat(new FizzBuzz().play(1)).isEqualTo("1");
+    public void count(){
+       FizzBuzz fb1 = new FizzBuzz();
+        assertThat(fb1.playNumber(1)).isEqualTo("1");
+
+
     }
 
-}
+    @Test
+    public void count2(){
+
+        FizzBuzz fb1 = new FizzBuzz();
+        assertThat(fb1.playNumber(2)).isEqualTo("2");
+
+    }
+    @Test
+    public void count3(){
+
+        FizzBuzz fb1 = new FizzBuzz();
+        assertThat(fb1.playNumber(3)).isEqualTo("fizz");
+
+    }
+
+    @Test
+    public void count5(){
+
+        FizzBuzz fb1 = new FizzBuzz();
+        assertThat(fb1.playNumber(5)).isEqualTo("buzz");
+
+    }
+    @Test
+    public void count6(){
+
+        FizzBuzz fb1 = new FizzBuzz();
+        assertThat(fb1.playNumber(6)).isEqualTo("fizz");
+
+    }
+
+    @Test
+    public void count15() {
+
+        FizzBuzz fb1 = new FizzBuzz();
+        assertThat(fb1.playNumber(15)).isEqualTo("fizzbuzz");
+    }
+    }
